b1aaa42133559b6d2275376b56b8fdd657dc2951
vishwa_patil <vishwapatil690@gmail.com>
2026-09-14 02:13:54 +0530

    Refactor Employee class structure and formatting

--- Assignments/OOPJ/Assignment/Employee/src/Employee.java
+++ Assignments/OOPJ/Assignment/Employee/src/Employee.java
@@ -5,15 +5,11 @@ public abstract class Employee {
     protected int age;
     protected boolean gender;
     protected float basicSalary;
+    
 
-
-    // Parameterized Constructor
-
-    public Employee(String name, String address, int age,
-                    boolean gender, float basicSalary) {
+    public Employee(String name, String address, int age, boolean gender, float basicSalary) {
 
         super();
-
         setName(name);
         setAddress(address);
         setAge(age);
@@ -21,53 +17,38 @@ public abstract class Employee {
         setBasicSalary(basicSalary);
     }
 
-
-    // Getter and Setter for Name
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-
         if (name == null || name.isEmpty())
             this.name = "Unknown";
         else
             this.name = name;
     }
 
-
-    // Getter and Setter for Address
-
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
-
         if (address == null || address.isEmpty())
             this.address = "Unknown";
         else
             this.address = address;
     }
 
-
-    // Getter and Setter for Age
-
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
-
         if (age < 18 || age > 65)
             this.age = 21;
         else
             this.age = age;
     }
-
-
-    // Getter and Setter for Gender
 
     public boolean getGender() {
         return gender;
@@ -77,24 +58,18 @@ public abstract class Employee {
         this.gender = gender;
     }
 
-
-    // Getter and Setter for Basic Salary
-
     public float getBasicSalary() {
         return basicSalary;
     }
 
     public void setBasicSalary(float basicSalary) {
-
         if (basicSalary <= 0)
             this.basicSalary = 10000;
         else
             this.basicSalary = basicSalary;
     }
 
-
     public void display() {
-
         System.out.println("Name         : " + name);
         System.out.println("Address      : " + address);
         System.out.println("Age          : " + age);
