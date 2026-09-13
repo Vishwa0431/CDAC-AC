32fd6de7fcc7d660f2069fdb99c04dd7f61a70c1
vishwa_patil <vishwapatil690@gmail.com>
2026-09-14 02:16:21 +0530

    Update Engineer.java

--- Assignments/OOPJ/Assignment/Employee/src/Engineer.java
+++ Assignments/OOPJ/Assignment/Employee/src/Engineer.java
@@ -1,14 +1,11 @@
 public class Engineer extends Employee {
 
-    public Engineer(String name, String address, int age,
-                    boolean gender, float basicSalary) {
-
+    public Engineer(String name, String address, int age, boolean gender, float basicSalary) {
         super(name, address, age, gender, basicSalary);
     }
 
     @Override
     public void display() {
-
         System.out.println("\n======Engineer=======");
 
         System.out.println("Name         : " + getName());
