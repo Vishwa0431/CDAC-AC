75aa8222f27ca944997e8b0e3b72b2a4703e03a8
vishwa_patil <vishwapatil690@gmail.com>
2026-09-14 02:15:49 +0530

    Update Manager.java

--- Assignments/OOPJ/Assignment/Employee/src/Manager.java
+++ Assignments/OOPJ/Assignment/Employee/src/Manager.java
@@ -1,14 +1,11 @@
 public class Manager extends Employee {
 
-    public Manager(String name, String address, int age,
-                   boolean gender, float basicSalary) {
-
+    public Manager(String name, String address, int age, boolean gender, float basicSalary) {
         super(name, address, age, gender, basicSalary);
     }
 
     @Override
     public void display() {
-
         System.out.println("\n=======Manager========");
 
         System.out.println("Name         : " + getName());
